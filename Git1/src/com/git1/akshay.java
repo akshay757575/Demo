7770fd8ed2ee95f8f681b1aa4f859161e6e05935
Admin <Admin@DESKTOP-2HQUVUD>
2023-03-03 16:45:59 +0530

    By sgffhgj

---
+++ Git1/src/com/git1/akshay.java
@@ -0,0 +1,17 @@
+package com.git1;
+
+public class akshay {
+
+	public static void main(String[] args) {
+
+		System.out.println("akshay1Git");
+
+		System.out.println("akshay54684");
+		System.out.println("akshaynghg5555");
+
+		System.out.println("akshay54684");
+		System.out.println("akshaynghg5555");
+
+	}
+
+}
--- git_program/src/com/demo/Demo.java
+++ git_program/src/com/demo/Demo.java
@@ -1,12 +1,11 @@
 package com.demo;
 
-public class Demo 
-{
-	public static void main(String[] args) 
-	{
+public class Demo {
+	public static void main(String[] args) {
 		System.out.println("Hello Rane");
 		System.out.println("HGCJHSDVJH");
 		System.out.println("Done...!");
+
 	}
 
 }
